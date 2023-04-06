ad3f21f9d4654ec9b73a1c72e8007381cd99214e
dgvalerio <al.davigvalerio@gmail.com>
2023-04-06 14:42:34 -0300

    feat(page): add base of home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,62 @@
 import { NextPage } from 'next';
 
-import { Button, Typography } from '@mui/material';
+import {
+  DarkMode as DarkModeIcon,
+  LightMode as LightModeIcon,
+} from '@mui/icons-material';
+import { Button, IconButton, Typography } from '@mui/material';
+
+import styled from '@emotion/styled';
 
 import useUiStore from '@/store/ui/store';
 
+const Container = styled.main`
+  &,
+  main {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+
+  main {
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
 const Home: NextPage = () => {
-  const { switchThemeMode } = useUiStore();
+  const { switchThemeMode, themeMode } = useUiStore();
+
+  const handleLogin = (): void => console.log('handleLogin');
 
   return (
-    <main>
-      <Typography variant="h1">The Helper</Typography>
-      <Button onClick={switchThemeMode}>Switch Theme</Button>
-    </main>
+    <Container>
+      <main>
+        <Typography variant="h2" component="h1">
+          Bem-vindo ao The Helper
+        </Typography>
+        <Typography variant="h4" component="p">
+          Antes de tudo, faça login para continuar:
+        </Typography>
+        <br />
+        <Button onClick={handleLogin}>Login</Button>
+      </main>
+      <footer>
+        <IconButton size="large" color="inherit" onClick={switchThemeMode}>
+          {themeMode === 'dark' ? (
+            <LightModeIcon color="disabled" />
+          ) : (
+            <DarkModeIcon color="disabled" />
+          )}
+        </IconButton>
+      </footer>
+    </Container>
   );
 };
 
--- store/ui/types.ts
+++ store/ui/types.ts
@@ -2,6 +2,6 @@ import { Theme } from '@mui/material';
 
 export interface UiStoreTypes {
   theme: Theme;
-
+  themeMode: Theme['palette']['mode'];
   switchThemeMode(): void;
 }
