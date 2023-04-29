933afe411a783afb62bbbcd03ec61c69d49be22d
dgvalerio <al.davigvalerio@gmail.com>
2023-04-29 20:35:04 -0300

    style(load-commits): use correct styles in form

--- src/github/commit/read/styles.tsx
+++ src/github/commit/read/styles.tsx
@@ -2,9 +2,7 @@ import styled from '@emotion/styled';
 
 export const CommitsSearchContainer = styled.div`
   display: flex;
-  flex: 2;
-
-  @media (min-width: ${({ theme }): number => theme.breakpoints.values.xl}px) {
-    flex: 4;
-  }
+  flex: 1;
+  height: calc(100vh - 4rem);
+  align-items: center;
 `;
