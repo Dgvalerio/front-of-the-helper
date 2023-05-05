0edae5e7a1dab2534440e518ca526256004761ce
dgvalerio <al.davigvalerio@gmail.com>
2023-05-05 10:36:22 -0300

    feat(loading): add option to show texts when loading

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,14 +1,61 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+
+import { Fade, Typography } from '@mui/material';
 
 import Styles from '@components/loading/styles';
 
-const Loading: FC = () => (
-  <Styles.Container>
-    <div id="clock" aria-label="Loading">
-      <div id="hour-hand" />
-      <div id="minute-hand" />
-    </div>
-  </Styles.Container>
-);
+const WithText: FC<{ texts: string[] }> = ({ texts }) => {
+  const [fade, setFade] = useState(true);
+  const [current, setCurrent] = useState(0);
+
+  const updateMessage = useCallback(
+    (): NodeJS.Timeout =>
+      setTimeout(() => {
+        setFade(false);
+        setTimeout(() => {
+          setCurrent(() => (current + 1) % texts.length);
+          setFade(true);
+        }, 200);
+      }, 2000),
+    [current, texts]
+  );
+
+  useEffect(() => {
+    const timeout = updateMessage();
+
+    return () => clearTimeout(timeout);
+  }, [updateMessage]);
+
+  return (
+    <>
+      <Styles.Container>
+        <div id="clock" aria-label="Loading">
+          <div id="hour-hand" />
+          <div id="minute-hand" />
+        </div>
+        <div id="text">
+          <Fade in={fade} unmountOnExit>
+            <Typography>{texts[current]}</Typography>
+          </Fade>
+        </div>
+      </Styles.Container>
+    </>
+  );
+};
+
+const Loading: FC<{ texts?: string[] }> = ({ texts }) => {
+  if (!texts) {
+    return (
+      <Styles.Container>
+        <div id="clock" aria-label="Loading">
+          <div id="hour-hand" />
+          <div id="minute-hand" />
+        </div>
+      </Styles.Container>
+    );
+  }
+
+  return <WithText texts={texts} />;
+};
 
 export default Loading;
